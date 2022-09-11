787553580d9f9033fabc2876e9a4d2562470a786
Aroop Prateek Singh <113294022+AroopPrateekSingh@users.noreply.github.com>
2022-09-11 20:52:21 +0530

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -2,10 +2,7 @@ from pprint import pprint
 
 
 def find_next_empty(puzzle):
-    # finds the next row, col on the puzzle that's not filled yet --> rep with -1
-    # return row, col tuple (or (None, None) if there is none)
 
-    # keep in mind that we are using 0-8 for our indices
     for r in range(9):
         for c in range(9): # range(9) is 0, 1, 2, ... 8
             if puzzle[r][c] == -1:
@@ -14,21 +11,12 @@ def find_next_empty(puzzle):
     return None, None  # if no spaces in the puzzle are empty (-1)
 
 def is_valid(puzzle, guess, row, col):
-    # figures out whether the guess at the row/col of the puzzle is a valid guess
-    # returns True or False
-
-    # for a guess to be valid, then we need to follow the sudoku rules
-    # that number must not be repeated in the row, column, or 3x3 square that it appears in
 
     # let's start with the row
     row_vals = puzzle[row]
     if guess in row_vals:
-        return False # if we've repeated, then our guess is not valid!
+        return False 
 
-    # now the column
-    # col_vals = []
-    # for i in range(9):
-    #     col_vals.append(puzzle[i][col])
     col_vals = [puzzle[i][col] for i in range(9)]
     if guess in col_vals:
         return False
@@ -45,12 +33,7 @@ def is_valid(puzzle, guess, row, col):
     return True
 
 def solve_sudoku(puzzle):
-    # solve sudoku using backtracking!
-    # our puzzle is a list of lists, where each inner list is a row in our sudoku puzzle
-    # return whether a solution exists
-    # mutates puzzle to be the solution (if solution exists)
     
-    # step 1: choose somewhere on the puzzle to make a guess
     row, col = find_next_empty(puzzle)
 
     # step 1.1: if there's nowhere left, then we're done because we only allowed valid inputs
